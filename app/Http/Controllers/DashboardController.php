ac13d999b6d1834ef4ddb653bc39a45e3d4579dc
Deris Ganesha Pratama <derisrplb15@gmail.com>
2023-07-03 14:25:09 +0700

    template dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $tot_lunas = DB::table('payment')->where('status',1)->count();
+        $tot_blm_lunas = DB::table('payment')->where('status',0)->count();
+        $tot_user = DB::table('users')->where('email','!=','admin@gmail.com')->count();
+        $tot_pendapatan = DB::table('detail_payment')->join('payment','payment.id','=','detail_payment.id_payment')->select(DB::raw('SUM(totalharga) as total_harga'))->where('status',1)->get()[0]->total_harga;
+        return view('dashboard.index',[
+            'tot_lunas' => $tot_lunas,
+            'tot_blm_lunas' => $tot_blm_lunas,
+            'tot_user' => $tot_user,
+            'tot_pendapatan' => $tot_pendapatan,
+        ]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,14 +5,171 @@
 @section('content_header')
     <h1 class="m-0 text-dark">Dashboard</h1>
 @stop
+@section('plugins.Chartjs',true)
 @section('content')
 <div class="row">
     <div class="col-12">
         <div class="card">
             <div class="card-body">
-                <h1>Dashboard</h1>
+                <div class="row">
+                    <div class="col-3">
+                        <div class="small-box bg-success">
+                            <div class="inner">
+                                <h3>{{$tot_lunas}}</h3>
+                                <p>Lunas</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-check"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="small-box bg-danger">
+                            <div class="inner">
+                                <h3>{{$tot_blm_lunas}}</h3>
+                                <p>Belum Lunas</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-times"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="small-box bg-info">
+                            <div class="inner">
+                                <h3>{{$tot_user}}</h3>
+                                <p>Customer</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-user"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="small-box bg-primary">
+                            <div class="inner">
+                                <h3>Rp.{{$tot_pendapatan == null ? '0' : $tot_pendapatan}}</h3>
+                                <p>Total Pemasukan</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-money-bill-alt"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="card card-success">
+            <div class="card-header">
+                <h3 class="card-title">Chart Penjualan Jersey Berdasarkan Liga</h3>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="chart">
+                    <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6">
+        <div class="card card-danger">
+            <div class="card-header">
+                <h3 class="card-title">Chart Jumlah Jersey Berdasarkan Liga</h3>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                        <i class="fas fa-times"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+                <canvas id="pieChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('js')
+    <script>
+        var areaChartData = {
+            labels  : ['Laliga', 'Premier League', 'Serie A', 'Bundesliga'],
+            datasets: [
+                {
+                    label               : 'Terjual',
+                    backgroundColor     : 'rgba(60,141,188,0.9)',
+                    borderColor         : 'rgba(60,141,188,0.8)',
+                    pointRadius          : false,
+                    pointColor          : '#3b8bba',
+                    pointStrokeColor    : 'rgba(60,141,188,1)',
+                    pointHighlightFill  : '#fff',
+                    pointHighlightStroke: 'rgba(60,141,188,1)',
+                    data                : [28, 48, 40, 19]
+                }
+            ]
+        }
+
+        var donutData = {
+            labels: [
+                'Chrome',
+                'IE',
+                'FireFox',
+                'Safari',
+                'Opera',
+                'Navigator',
+            ],
+            datasets: [
+                {
+                    data: [700,500,400,600,300,100],
+                    backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+                }
+            ]
+        }
+
+        //-------------
+        //- BAR CHART -
+        //-------------
+        var barChartCanvas = $('#barChart').get(0).getContext('2d')
+        var barChartData = $.extend(true, {}, areaChartData)
+        var temp0 = areaChartData.datasets[0]
+        barChartData.datasets[0] = temp0
+
+        var barChartOptions = {
+            responsive              : true,
+            maintainAspectRatio     : false,
+            datasetFill             : false
+        }
+
+        new Chart(barChartCanvas, {
+            type: 'bar',
+            data: barChartData,
+            options: barChartOptions
+        })
+
+        //-------------
+        //- PIE CHART -
+        //-------------
+        // Get context with jQuery - using jQuery's .get() method.
+        var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+        var pieData        = donutData;
+        var pieOptions     = {
+            maintainAspectRatio : false,
+            responsive : true,
+        }
+        //Create pie or douhnut chart
+        // You can switch between pie and douhnut using the method below.
+        new Chart(pieChartCanvas, {
+            type: 'pie',
+            data: pieData,
+            options: pieOptions
+        })
+    </script>
 @endsection
