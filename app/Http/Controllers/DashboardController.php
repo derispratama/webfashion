ece3d89018bbb4e916f94cebff3fb17d55d89bdf
muhammad ridho asyari <mridhoasyari@MacBook-Air-muhammad.local>
2023-07-04 16:44:32 +0700

    update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -23,7 +23,7 @@ class DashboardController extends Controller
     }
 
     public function chart(){
-        $payment = DB::select('SELECT l.id as id_liga, l.nama as nama_liga, SUM(p.stok) as totalstok FROM produk p INNER JOIN liga l ON l.id = p.id_liga GROUP BY l.id;');
+        $payment = DB::select('SELECT l.id as id_liga, l.nama as nama_liga, SUM(p.stok) as totalstok FROM produk p INNER JOIN liga l ON l.id = p.id_liga GROUP BY l.id,l.nama;');
         echo json_encode(['data' => $payment]);
     }
 }
